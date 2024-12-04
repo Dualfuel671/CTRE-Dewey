1d8de917aaa0e4a144c2a639858e835b71d7182d
Dualfuel671 <brucejackson1000@gmail.com>
2024-12-04 15:49:00 -0500

    Initial commit of DriveCode-ImportedII

---
+++ src/main/java/frc/robot/NetworkTables.java
@@ -0,0 +1,21 @@
+package frc.robot;
+
+import edu.wpi.first.networktables.BooleanSubscriber;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class NetworkTables {
+        NetworkTableInstance inst = NetworkTableInstance.getDefault();
+        final NetworkTable table = inst.getTable("Laptop");
+        BooleanSubscriber testBoolean;
+
+    public NetworkTables(){
+
+        testBoolean = table.getBooleanTopic("testBoolean").subscribe(false);
+    }
+
+
+    public boolean getTestEntry(){
+        return testBoolean.get();
+    }
+}
